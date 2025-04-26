4b1c6493d61591fabc8e3236df9edb0ad81c5ef4
DataWidgetDesigns <datawidgetdesigns@gmail.com>
2025-04-26 15:19:34 +0200

    Update viz-codelab

--- viz-codelab.js
+++ viz-codelab.js
@@ -36,8 +36,38 @@ function drawViz(data) {
     document.body.appendChild(container);
   }
 
-  // Render the viz.
-  container.textContent = 'Hello, viz world!';
+  // Create the table.
+  const table = document.createElement('table');
+  const tableHeader = document.createElement('thead');
+  const tableBody = document.createElement('tbody');
+
+  data.tables.DEFAULT.headers.forEach(function (column) {
+    const tableColumn = document.createElement('th');
+    tableColumn.textContent = column.name;
+    tableHeader.appendChild(tableColumn);
+  });
+
+  data.tables.DEFAULT.rows.forEach(function(row) {
+    const tableRow = document.createElement('tr');
+    row.forEach(function(cell) {
+      const tableCell = document.createElement('td');
+      if (typeof cell == 'number') {
+        tableCell.textContent = new Intl.NumberFormat().format(cell);
+      } else {
+        tableCell.textContent = cell;
+      }
+      tableRow.appendChild(tableCell);
+    });
+    tableBody.appendChild(tableRow);
+  });
+  table.appendChild(tableHeader);
+  table.appendChild(tableBody);
+
+  // Set header color based on style control.
+  tableHeader.style.backgroundColor = data.style.headerBg.value.color;
+
+  // Render the table.
+  container.appendChild(table);
 
 }
 
